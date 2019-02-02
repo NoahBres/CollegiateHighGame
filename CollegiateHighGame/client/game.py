9d5200ee27c075d75ab75f094ed72d4170de51c6
Noah <noahbres@gmail.com>
2019-02-02 00:30:00 -0600

    Add client and game.py

---
+++ CollegiateHighGame/client/game.py
@@ -0,0 +1,64 @@
+from sys import exit
+import pygame
+from pygame import locals
+
+(width, height) = (800, 600)
+background = (0, 0, 0)
+
+clock = pygame.time.Clock()
+ticks_per_second = 60
+
+
+class Game:
+    def __init__(self, address, tcp_port, udp_port, udp_address):
+        self.running = False
+
+        self.tcp_port = tcp_port
+        self.udp_port = udp_port
+        self.udp_address = udp_address
+
+    def run(self):
+        # self.connect()
+
+        pygame.init()
+
+        screen = pygame.display.set_mode((width, height))
+        pygame.display.set_caption("Collegiate Tech Club Game")
+
+        self.running = True
+
+        try:
+            while self.running:
+                clock.tick(ticks_per_second)
+
+                self.poll_events()
+                self.update()
+                self.draw(screen)
+
+            pygame.quit()
+            exit(0)
+        except SystemExit():
+            pygame.quit()
+            exit(0)  # Redudant but ¯\_(ツ)_/¯
+
+    def poll_events(self):
+        events = pygame.event.get()
+        # keys = pygame.key.get_pressed()
+
+        # Check for quit
+        for event in events:
+            if event.type == locals.QUIT:
+                self.running = False
+                return
+            if event.type == locals.KEYDOWN:
+                if event.key == locals.K_ESCAPE:
+                    self.running = False
+                    return
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        screen.fill(background)
+
+        pygame.display.flip()
