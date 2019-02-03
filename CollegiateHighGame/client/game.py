8e0e6ce8b1b4987135c662b47f25baec209c7e10
Noah <noahbres@gmail.com>
2019-02-03 03:30:25 -0600

    Add registering fellow clients

--- CollegiateHighGame/client/game.py
+++ CollegiateHighGame/client/game.py
@@ -3,6 +3,7 @@ import pygame
 from pygame import locals
 
 from .network_connector import NetworkConnector
+from .player import Player
 
 (width, height) = (800, 600)
 background = (0, 0, 0)
@@ -13,12 +14,16 @@ ticks_per_second = 60
 
 class Game:
     def __init__(self, address, tcp_port, udp_port, udp_address):
-        self.connector = NetworkConnector(address, tcp_port, udp_port, udp_address)
+        self.connector = NetworkConnector(
+            address, tcp_port, udp_port, udp_address, self
+        )
         self.running = False
 
         self.tcp_port = tcp_port
         self.udp_port = udp_port
         self.udp_address = udp_address
+
+        self.clients = {}
 
     def run(self):
         self.connector.register()
@@ -30,6 +35,8 @@ class Game:
 
         screen = pygame.display.set_mode((width, height))
         pygame.display.set_caption(f"Collegiate Tech Club Game - {self.connector.id}")
+
+        self.player = Player()
 
         self.running = True
 
@@ -64,9 +71,20 @@ class Game:
                     return
 
     def update(self):
-        pass
+        self.player.update()
 
     def draw(self, screen):
         screen.fill(background)
 
+        self.player.draw(screen)
+
         pygame.display.flip()
+
+    def register_clients(self, client_list):
+        print(client_list)
+
+        new_items = set(client_list) - self.clients.keys()
+
+        for item in new_items:
+            if item != self.connector.id:
+                print(f"fellow client: {item}")
