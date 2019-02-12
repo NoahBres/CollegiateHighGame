012fcb747d2e76ca10c41cdb58d635bc269e3436
Noah <noahbres@gmail.com>
2019-02-12 13:12:55 -0600

    Draw title and text in IntroState

--- CollegiateHighGame/states/intro_state.py
+++ CollegiateHighGame/states/intro_state.py
@@ -3,8 +3,9 @@ from .state import State
 
 
 class IntroState(State):
-    def __init__(self):
-        State.__init__(self)
+    def __init__(self, *args, **kwargs):
+        # State.__init__(self)
+        super().__init__(*args, **kwargs)
 
         # self.title = {
         #     "size": 60,
@@ -13,17 +14,58 @@ class IntroState(State):
         #     "font": pygame.font.SysFont(None, self.title["size"]),
         # }
 
+        self.title = TextObject()
+        self.title.text = "Test Title"
+        self.title.size = 60
+        self.title.centerx = self.game.center_width
+        self.title.centery = self.game.center_height - self.game.height / 4
+
+        self.joystick_connected_text = TextObject()
+        self.joystick_connected_text.text = (
+            f"{self.game.joystick_count} Joysticks Connected"
+        )
+        self.joystick_connected_text.size = 20
+        self.joystick_connected_text.centerx = self.game.center_width
+        self.joystick_connected_text.centery = self.game.center_height
+
     def update(self):
         pass
 
     def draw(self, screen):
         screen.fill((0, 0, 0))
 
-        # title = self.title["font"].render("Test Title", True, self.title_color)
+        self.title.draw(screen)
+        self.joystick_connected_text.draw(screen)
 
-        # title_rect = title.get_rect()
-        # title_rect.centerx = screen.get_rect().centerx
-        # title_rect.centery = screen.get_rect().centery - self.title_offset_y
 
-        # screen.blit(title, title_rect)
+class TextObject:
+    def __init__(self):
+        self.text = ""
+        self.size = 14
+        self.color = (255, 255, 255)
+        self.font = pygame.font.SysFont(None, self.size)
+        self.centerx = 0
+        self.centery = 0
 
+    def draw(self, surface):
+        text = self.font.render(self.text, True, self.color)
+
+        text_rect = text.get_rect()
+        text_rect.centerx = self.centerx
+        text_rect.centery = self.centery
+
+        surface.blit(text, text_rect)
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, size):
+        self.__size = size
+        self.font = pygame.font.SysFont(None, self.size)
+
+
+class Button:
+    def __init__(self):
+        pass
