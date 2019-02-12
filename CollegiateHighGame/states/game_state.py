bbc07cceb3a413372ef2f1539ed173a47207fbe6
Noah <noahbres@gmail.com>
2019-02-12 16:24:05 -0600

    Create divider in game

--- CollegiateHighGame/states/game_state.py
+++ CollegiateHighGame/states/game_state.py
@@ -4,11 +4,23 @@ from pygame import locals
 from .state import State
 
 
+white = (255, 255, 255)
+
+
 class GameState(State):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
         self.background = (29, 25, 35)
+        self.divider_width = 4
 
     def draw(self, screen):
         screen.fill(self.background)
+
+        divider = pygame.Rect(
+            self.game.center_width - self.divider_width / 2,
+            0,
+            self.divider_width,
+            self.game.height,
+        )
+        screen.fill(white, divider)
