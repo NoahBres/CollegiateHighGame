4aaeb5cb04e3612336b4dd246d069573c7aa74da
Noah <noahbres@gmail.com>
2019-02-12 22:28:42 -0600

    Add player and player views

--- CollegiateHighGame/states/game_state.py
+++ CollegiateHighGame/states/game_state.py
@@ -2,7 +2,7 @@ import pygame
 from pygame import locals
 
 from .state import State
-
+from CollegiateHighGame.entities.player import Player
 
 white = (255, 255, 255)
 
@@ -14,8 +14,32 @@ class GameState(State):
         self.background = (29, 25, 35)
         self.divider_width = 4
 
+        player_view_dimensions = ((int(self.game.width / 2)), int(self.game.height))
+
+        self.player1_view = pygame.Surface(player_view_dimensions)
+        self.player1_view.fill((0, 255, 0))
+        self.player1_view.set_colorkey((0, 255, 0))
+
+        self.player2_view = pygame.Surface(player_view_dimensions)
+        self.player2_view.fill((0, 255, 0))
+        self.player2_view.set_colorkey((0, 255, 0))
+
+        self.player_group = pygame.sprite.Group()
+
+        self.player1 = Player()
+        self.player_group.add(self.player1)
+
+    def update(self):
+        pass
+
     def draw(self, screen):
         screen.fill(self.background)
+
+        # self.player_group.draw(screen)
+        self.player1_view.blit(self.player1.image, (800, 0))
+
+        screen.blit(self.player1_view, (0, 0))
+        screen.blit(self.player2_view, (self.game.center_width, 0))
 
         divider = pygame.Rect(
             self.game.center_width - self.divider_width / 2,
