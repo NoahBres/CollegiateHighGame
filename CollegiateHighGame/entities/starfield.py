37a12f9a235604445966c6e8ec81ef88e4210b04
Noah <noahbres@gmail.com>
2019-02-19 22:47:37 -0800

    Prefill stars rather than update as you move through the world

--- CollegiateHighGame/entities/starfield.py
+++ CollegiateHighGame/entities/starfield.py
@@ -1,5 +1,7 @@
 from random import randrange
+import time
 import pygame
+from pygame.math import Vector2
 
 from .entity import Entity
 from CollegiateHighGame.util.hash_map import HashMap
@@ -13,44 +15,61 @@ class Starfield(Entity):
         self.hash_map = HashMap(self.cell_size)
         self.player_views = player_views
 
-        self.max_stars = 12
+        self.max_stars = 30
 
     def update(self):
         # print(self.player_views[0].coords)
         # print(self.player_views[1].coords)
         # print(self.hash_map.grid)
 
-        for view in self.player_views:
-            query = self.hash_map.query_area(
-                view.coords.topleft, view.coords.width, view.coords.height
-            )
-            for cell, point in query:
-                if len(cell) == 0:
-                    # print(cell)
-                    self.fill_with_stars(point)
+        # for view in self.player_views:
+        #     query = self.hash_map.query_area(
+        #         view.coords.topleft, view.coords.width, view.coords.height
+        #     )
+        #     for cell, point in query:
+        #         if len(cell) == 0:
+        #             # print(cell)
+        #             self.fill_with_stars(point)
+
+        pass
 
     def draw(self):
-        calls = 0
         for view in self.player_views:
             query = self.hash_map.query_area(
                 view.coords.topleft, view.coords.width, view.coords.height
             )
-            # print(len(query))
             for cell, point in query:
                 if len(cell) != 0:
                     for pos in cell:
-                        calls += 1
                         # print(pos)
-            # pygame.draw.rect(view.surface, (255, 255, 255), pygame.Rect(pos[0], pos[1], 2, 2))
+                        offset_coords = Vector2(pos) - Vector2(
+                            view.coords.x, view.coords.y
+                        )
+                        pygame.draw.rect(
+                            view.surface,
+                            (255, 255, 255),
+                            pygame.Rect(offset_coords[0], offset_coords[1], 2, 2),
+                        )
             # pygame.draw.circle(view.surface, (255, 255, 255), pos, 1)
             # print(cell)
             # self.fill_with_stars(point)
-        print(calls)
 
     def fill_with_stars(self, point):
+        print("Fill me")
         for i in range(randrange(self.max_stars)):
             x, y = point
             x += randrange(self.cell_size)
             y += randrange(self.cell_size)
 
             self.hash_map.add([x, y], (x, y))
+
+    def prefill(self, star_count, max_x, max_y):
+        print("Filling the sky with stars...")
+        start = time.clock()
+        for i in range(star_count):
+            x = randrange(max_x)
+            y = randrange(max_y)
+
+            self.hash_map.add([x, y], (x, y))
+        passed = time.clock() - start
+        print(f"The sky is lit. Took {passed} seconds")
