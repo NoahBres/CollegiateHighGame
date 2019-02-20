3af8b7a604be5fddad644e615e126617df4bc956
Noah <noahbres@gmail.com>
2019-02-20 00:49:11 -0600

    Change dictionary to defaultdict

--- CollegiateHighGame/util/hash_map.py
+++ CollegiateHighGame/util/hash_map.py
@@ -1,10 +1,18 @@
-from math import floor
+import itertools
+from math import ceil
+from collections import defaultdict
+
+import time
 
 
 class HashMap:
     def __init__(self, cell_size):
         self.cell_size = cell_size
-        self.grid = {}
+        self.grid = defaultdict(list)
+
+        self.last_query_list_length = 2
+        self.last_area_query_points = []
+        self.last_area_query_list = []
 
     def key(self, point):
         return (
@@ -14,23 +22,34 @@ class HashMap:
 
     def add(self, obj, point):
         key = self.key(point)
-        if self.grid.get(key) is None:
-            self.grid[key] = []
         self.grid[key].append(obj)
-        # self.grid.get(key, []).append(obj)
 
     def query_point(self, point):
         key = self.key(point)
+
+        # print(f"Querying: {key}")
+
         return self.grid.get(key, [])
 
     def query_area(self, point, width, height):
+        # start = time.clock()
+
         squares_to_query = []
-        for x in range(floor(width / self.cell_size)):
-            for y in range(floor(height / self.cell_size)):
-                query_x = point[0] + x * self.cell_size
-                query_y = point[1] + y * self.cell_size
-                squares_to_query.append(
-                    [self.query_point((query_x, query_y)), (query_x, query_y)]
-                )
+        # These 2 loops almost make no difference. Thought it would be faster tho
+        # for x in range(ceil(width / self.cell_size)):
+        # for y in range(ceil(height / self.cell_size)):
+        for x, y in itertools.product(
+            range(ceil(width / self.cell_size) + 1),
+            range(ceil(height / self.cell_size) + 1),
+        ):
+            query_x = point[0] + x * self.cell_size
+            query_y = point[1] + y * self.cell_size
+
+            squares_to_query.append(
+                [self.query_point((query_x, query_y)), (query_x, query_y)]
+            )
+
+        # passed = time.clock() - start
+        # print(f"Query took {passed * 1000} ms")
 
         return squares_to_query
