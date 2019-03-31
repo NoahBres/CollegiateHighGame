2bbd327471dbc99aa7a81db0b066465df254b6d0
Noah <noahbres@gmail.com>
2019-03-31 14:49:53 -0500

    Add turret targetting

--- CollegiateHighGame/entities/player_base_turret.py
+++ CollegiateHighGame/entities/player_base_turret.py
@@ -1,4 +1,5 @@
 import os
+from math import degrees, atan2
 
 import pygame
 from pygame.math import Vector2
@@ -31,18 +32,30 @@ class PlayerBaseTurret(pygame.sprite.Sprite, Entity):
         )
         self.rect = self.foundation_image.get_rect()
 
-        self.turret_image = pygame.image.load(turret_path).convert_alpha()
+        self.orig_turret_image = pygame.image.load(turret_path).convert_alpha()
+        self.turret_image = self.orig_turret_image.copy()
         self.orig_turret_rect = self.turret_image.get_rect()
         self.turret_rect = self.turret_image.get_rect()
 
         self.world_pos = Vector2(x, y)
-        self.angle = 0
+        self.turret_angle = 0
+        self.turret_target_angle = 0
+
+        self.last_turret_target_angle = 0
+
+        self.turret_speed = 1
 
         self.base = base
         self.game = game
 
     def update(self, delta_time):
-        pass
+        if self.turret_angle * 2 > self.turret_target_angle * 2:
+            self.turret_angle -= self.turret_speed / 16 * delta_time
+            self.set_turret_angle(self.turret_angle)
+        elif self.turret_angle * 2 < self.turret_target_angle * 2:
+            self.turret_angle += self.turret_speed / 16 * delta_time
+            self.set_turret_angle(self.turret_angle)
+        # self.turret_angle
 
     def draw(self, surface, coords=None):
         rect = self.rect.copy()
@@ -61,3 +74,28 @@ class PlayerBaseTurret(pygame.sprite.Sprite, Entity):
             self.orig_foundation_image, self.foundation_angle
         )
         self.rect = self.foundation_image.get_rect(center=self.rect.center)
+
+    def set_turret_angle(self, angle):
+        self.turret_angle = angle
+
+        self.turret_image = pygame.transform.rotate(
+            self.orig_turret_image, self.turret_angle
+        )
+        self.turret_rect = self.turret_image.get_rect(center=self.turret_rect.center)
+
+    def target(self, point):
+        # self.set_turret_angle(90)
+        # self.turret_target_angle = degrees(self.world_pos.angle_to(point))
+        # difference = point - self.world_pos
+        # self.turret_target_angle = 270 - degrees(atan2(difference.y, difference.x))
+        difference = self.world_pos - point
+        self.turret_target_angle = 90 - degrees(atan2(difference.y, difference.x))
+
+        # Values are arbitrarily set. Should probably make them variables
+        if self.turret_target_angle > 250 and self.last_turret_target_angle < -70:
+            self.turret_angle += 360
+        elif self.turret_target_angle < -70 and self.last_turret_target_angle > 250:
+            self.turret_angle -= 360
+            # self.turret_target_angle = -90 - (270 - self.turret_target_angle)
+
+        self.last_turret_target_angle = self.turret_target_angle
