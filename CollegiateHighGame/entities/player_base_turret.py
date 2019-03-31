1f547eaa7f86bf39514e5f4816cd9836e1af1c0f
Noah <noahbres@gmail.com>
2019-03-31 01:39:00 -0500

    Add a turret to the base

---
+++ CollegiateHighGame/entities/player_base_turret.py
@@ -0,0 +1,63 @@
+import os
+
+import pygame
+from pygame.math import Vector2
+
+from .entity import Entity
+
+
+class PlayerBaseTurret(pygame.sprite.Sprite, Entity):
+    def __init__(self, x, y, base, game):
+        pygame.sprite.Sprite.__init__(self)
+        Entity.__init__(self)
+
+        base_path = os.path.dirname(__file__)
+        base_path = os.path.abspath(os.path.join(base_path, os.path.pardir))
+
+        foundation_path = os.path.join(
+            base_path, "assets", "entities", "spaceBuilding_018.png"
+        )
+        turret_path = os.path.join(
+            base_path, "assets", "entities", "spaceBuilding_022.png"
+        )
+
+        self.orig_foundation_image = pygame.image.load(foundation_path).convert_alpha()
+        self.foundation_image = self.orig_foundation_image.copy()
+        self.orig_rect = self.foundation_image.get_rect()
+
+        self.foundation_angle = 45
+        self.foundation_image = pygame.transform.rotate(
+            self.foundation_image, self.foundation_angle
+        )
+        self.rect = self.foundation_image.get_rect()
+
+        self.turret_image = pygame.image.load(turret_path).convert_alpha()
+        self.orig_turret_rect = self.turret_image.get_rect()
+        self.turret_rect = self.turret_image.get_rect()
+
+        self.world_pos = Vector2(x, y)
+        self.angle = 0
+
+        self.base = base
+        self.game = game
+
+    def update(self, delta_time):
+        pass
+
+    def draw(self, surface, coords=None):
+        rect = self.rect.copy()
+        turret_rect = self.turret_rect.copy()
+        if coords is not None:
+            rect.center = coords
+            turret_rect.center = coords
+
+        surface.blit(self.foundation_image, rect)
+        surface.blit(self.turret_image, turret_rect)
+
+    def set_foundation_angle(self, angle):
+        self.foundation_angle = angle
+
+        self.foundation_image = pygame.transform.rotate(
+            self.orig_foundation_image, self.foundation_angle
+        )
+        self.rect = self.foundation_image.get_rect(center=self.rect.center)
