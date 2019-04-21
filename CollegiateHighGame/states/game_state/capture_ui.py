ab4a89ba303806d4c31a66b541ba29db302d9c92
Noah <noahbres@gmail.com>
2019-04-20 19:25:22 -0500

    Misnamed variable

--- CollegiateHighGame/states/game_state/capture_ui.py
+++ CollegiateHighGame/states/game_state/capture_ui.py
@@ -10,24 +10,23 @@ class CaptureUI:
         self.captures = captures
 
         self.padding = {"left": 10, "right": 10, "top": 5, "bottom": 5}
-        self.margin_flag = 10
+        self.margin_flag = 5
 
         base_path = os.path.dirname(__file__)
         base_path = os.path.abspath(
             os.path.join(base_path, os.path.pardir, os.path.pardir)
         )
 
-        image_path = os.path.join(
-            base_path, "assets", "entities", f"{sprite_name}.png"
-        )
+        image_path = os.path.join(base_path, "assets", "entities", f"{sprite_name}.png")
 
         self.image = pygame.image.load(image_path).convert_alpha()
 
         scale = 0.3
-        size = self.orig_image.get_size()
+        size = self.image.get_size()
 
         scaled_dimen = ((int(size[0] * scale)), int(size[1] * scale))
-        self.image = pygame.transform.smoothscale(self.orig_image, scaled_dimen)
+        self.image = pygame.transform.smoothscale(self.image, scaled_dimen)
+        self.image = pygame.transform.rotate(self.image, 45)
 
         self.rect = self.image.get_rect()
 
@@ -35,7 +34,7 @@ class CaptureUI:
         self.rect.y = self.point[1] - self.rect.height
 
         self.surface = pygame.Surface((self.rect.width, self.rect.height))
-        
+
         self.redraw()
 
     def set_captures(self, captures):
@@ -44,15 +43,17 @@ class CaptureUI:
 
     def redraw(self):
         self.rect.width = 0
-        self.rect.width += self.margin_flag * self.count - 1
-        self.rect.width += sum(self.padding['left'], self.padding['right'])
-        self.rect.width += self.image.get_width() * self.count
+        self.rect.width += self.margin_flag * self.captures - 1
+        self.rect.width += self.padding["left"] + self.padding["right"]
+        self.rect.width += self.image.get_width() * self.captures
 
-        self.surface = pygame.Surface((self.rect.width, self.rect.height), pygame.SRCALPHA)
+        self.surface = pygame.Surface(
+            (self.rect.width, self.rect.height), pygame.SRCALPHA
+        )
 
-        cursor = self.padding['left']
-        for i in range(self.count):
-            self.surface.blit(self.image, (cursor, self.padding['top']))
+        cursor = self.padding["left"]
+        for i in range(self.captures):
+            self.surface.blit(self.image, (cursor, self.padding["top"]))
             cursor += self.image.get_width() + self.margin_flag
 
     def draw(self, surface):
